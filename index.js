e2aee2d23f9b37bb8560f14fbebe688f791b3631
piratejas <47509562+piratejas@users.noreply.github.com>
2023-02-14 17:40:44 +0000

    disabled caching to fetch new advice slip on click; polished styles

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const adviceText = document.querySelector(".advice-text");
 const slipNumber = document.querySelector(".slip-number");
 
 async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice");
+  const response = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-cache",
+  });
   const data = await response.json();
   console.log(data);
   slipNumber.innerHTML = await data.slip.id;
